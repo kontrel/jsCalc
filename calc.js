9bdad5d16ef0e5e2a9ddcee968f90f42423b9f07
Kontrel <konradtrelek@gmail.com>
2021-09-10 15:58:15 +0200

    add js functions

---
+++ calc.js
@@ -0,0 +1,62 @@
+window.onload = function() {
+    console.log("app started")
+    calculator.init();
+}
+
+let calculator = {
+    buttons: undefined,
+    input: undefined,
+    init: function() {
+        this.buttons = document.querySelectorAll(".numbers button, .operators button");
+        this.input = document.getElementById("input");
+
+        for (let i = 0; i < this.buttons.length; i++) {
+            let el = this.buttons[i];
+            el.addEventListener("click", this.buttonClick);
+        }
+    },
+
+    buttonClick: function(e) {
+        let divHtmlText = e.target.innerHTML;
+        console.log("Klik "+divHtmlText);
+        switch(divHtmlText) {
+            case "=":
+                calculator.evaluates();
+            break;
+            case "C":
+                calculator.clear();
+            break;
+            case "9":
+            case "8":
+            case "7":
+            case "6":
+            case "5":
+            case "4":
+            case "3":
+            case "2":
+            case "1":
+            case "0":
+            case "00":
+            case ".":
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                calculator.addToInput(divHtmlText);
+            break;
+        };
+    },
+
+    addToInput: function(str) {
+        this.input.value += str;
+    },
+
+    evaluates: function() {
+        let result = math.evaluate(calculator.input.value);
+        calculator.input.value = result;
+    },
+
+    clear: function() {
+        calculator.input.value="";
+    }
+}
